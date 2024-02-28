3a732d2e9e701dbe0a6b58845e9dc8d83a8b29a7
chad <chadayop@gmail.com>
2024-02-28 22:50:21 +0800

    Fixed pages controller issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
-    @tasks_by_category = Task.where(due_date: Date.today, status_complete: false)
-    .includes(:category)
-    .group_by(&:category)
+    @tasks_by_category = current_user.tasks.where(due_date: Date.today, status_complete: false)
+      .includes(:category)
+      .group_by(&:category)
   end
 end
