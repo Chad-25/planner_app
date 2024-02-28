349397daa8c9476b859d2a998dfd4c8bdc8bb774
chad <chadayop@gmail.com>
2024-02-28 23:18:30 +0800

    Refactor controller tasks and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,18 +1,17 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user
+  before_action :set_category, only: [:edit, :update, :show, :destroy]
 
   def index
-    @user = current_user
     @categories = @user.categories
   end
 
   def new
-    @user = current_user
     @category = @user.categories.build
   end
 
   def create
-    @user = current_user
     @category = @user.categories.build(category_params)
     if @category.save
       redirect_to category_path(@category), notice: "Category created successfully"
@@ -23,14 +22,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @user = current_user
-    @category = @user.categories.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:user_id])
-    @category = @user.categories.find(params[:id])
-
     if @category.update(category_params)
       redirect_to user_category_path(@user, @category), notice: "Category was successfully updated."
     else
@@ -39,17 +33,22 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @user = current_user
-    @category = @user.categories.find(params[:id])
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
-    redirect_to @category, notice: "Category was successfully deleted."
+    redirect_to categories_path, notice: "Category was successfully deleted."
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_category
+    @category = @user.categories.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:title, :description)
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,22 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category, except: [:update_status]
+  before_action :set_task_update, only: [:update_status]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @category = current_user.categories.find(params[:category_id])
     @tasks = @category.tasks
   end
 
   def show
-    @category = current_user.categories.find(params[:category_id])
-    @task = @category.tasks.find_by(id: params[:id])
-
-    if @task.nil?
-      redirect_to category_path(@category), alert: "Task not found."
-    end
+    redirect_to category_path(@category), alert: "Task not found." if @task.nil?
   end
 
   def new
-    @category = current_user.categories.find(params[:category_id])
     @task = Task.new
   end
 
   def create
-    @category = current_user.categories.find(params[:category_id])
     @task = @category.tasks.build(task_params)
 
     if @task.save
@@ -32,14 +27,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @category = current_user.categories.find(params[:category_id])
-    @task = @category.tasks.find(params[:id])
   end
 
   def update
-    @category = current_user.categories.find(params[:category_id])
-    @task = @category.tasks.find(params[:id])
-
     if @task.update(task_params)
       redirect_to category_path(@category), notice: "Task status updated successfully."
     else
@@ -48,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def update_status
-    @task = Task.find(params[:id])
     if @task.update(task_params) && @task.status_complete
       flash[:notice] = "Wooh, good job!"
     end
@@ -56,8 +45,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @category = current_user.categories.find(params[:category_id])
-    @task = @category.tasks.find(params[:id])
     @task.destroy
     redirect_back(fallback_location: root_path, notice: "Task was successfully deleted.")
   end
@@ -67,8 +54,19 @@ class TasksController < ApplicationController
 
   private
 
+  def set_category
+    @category = current_user.categories.find(params[:category_id])
+  end
+
+  def set_task
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def set_task_update
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:name, :due_date, :status_complete)
   end
-
 end
