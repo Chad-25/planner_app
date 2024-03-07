190fa60f64e2997a25d79bd45cc14176db8331f0
chad <chadayop@gmail.com>
2024-03-07 20:05:02 +0800

    Fixed update_status_complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,16 @@ class Task < ApplicationRecord
   belongs_to :category
 
   validates :name, presence: { message: "Please enter a task." }
-  validate :due_date_not_in_past
+  validate :due_date_not_in_past, unless: :updating_status_complete?
 
   def due_date_not_in_past
     if due_date.present? && due_date < Date.today
       errors.add(:due_date, "Date can't be in the past.")
     end
   end
+
+  def updating_status_complete?
+    status_complete_changed? && !due_date_changed?
+  end
+
 end
