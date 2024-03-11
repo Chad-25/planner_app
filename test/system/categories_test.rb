1be3a3ab18998b2bdd2e6737b63a831dd5f1c96d
chad <chadayop@gmail.com>
2024-03-11 20:34:25 +0800

    Fix web-driver and system test

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,21 +1,34 @@
 require "application_system_test_case"
 
 class CategoriesTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  test "visiting the sign-in page" do
+    visit root_path
+    assert_current_path(new_user_session_path)
+  end
+
   test "creating a new category" do
     user = users(:one)
+    sign_in user
+
+    visit root_path
+
+    assert_text "HOORAY, NO TASK FOR TODAY!"
+    assert_text "CREATE TASK NOW"
+
+    link = find_link("Create Task Now", class: "category-add-button")
+    link.click
+    assert_current_path new_category_path
 
     category = Category.new(title: "Vacation", description: "Sample Description")
     category.user = user
 
-    visit root_path
-    click_link "New Category"
+    fill_in "category_title", with: category.title
+    fill_in "category_description", with: category.description
+    click_button "Submit"
 
-    fill_in "Title", with: category.title
-    fill_in "Description", with: category.description
-    click_button "Create Category"
-
-    assert_text "Category was successfully created."
-    assert_text category.title
-    assert_text category.description
+    assert_text "Category created successfully"
+    assert_text category.title.upcase
   end
 end
